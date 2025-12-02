6fbd2beb8d9bb76d90438fa9ccbaed896a0bd5ad
Danielle Samson <danielle.c.samson@gmail.com>
2025-12-01 18:11:31 -0700

    solved vs unsolved cases

--- script.R
+++ script.R
@@ -4,20 +4,20 @@ library(tigris)
 
 #read in the data
 homicides <- read.csv("homicide-data.csv")
+
 #choose a city
 print(unique(homicides$city))
 
 denver_homicides <- homicides %>%
-  filter(city == "Denver") %>% 
-  sf::st_as_sf(coords = c("lat", "lon"),
+  filter(city == 'Denver') %>% 
+  st_as_sf(coords = c("lon", "lat"),
                crs = 4269)
-#check converstion
+
 denver_homicides %>%
   ggplot() +
   geom_sf()
 
 #extract denver boundaries from tigris
-
 places <- tigris::places(state = "CO",
                                     cb = FALSE,
                                     year = "2024")
@@ -28,12 +28,23 @@ denver_boundaries %>%
   ggplot() +
   geom_sf()
 
-#check the boundaries
+#align the geometries
 ggplot() +
   geom_sf(data = denver_boundaries) +
   geom_sf(data = denver_homicides)
 
+#counts by race
+victim_race <- denver_homicides %>%
+  group_by(victim_race) %>%
+  summarise(
+    homicides_by_race = n()
+  ) %>%
+  arrange(desc(homicides_by_race)) %>%
+  slice(1:3) %>%
+  print()
 
+#filter to most murdered races
 
-
+denver_homicides_by_race <- denver_homicides %>%
+  filter(victim_race %in% c("Black", "White", "Hispanic"))
 
